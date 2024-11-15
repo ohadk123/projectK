ab8e31aa0cc8fd57eb9c1e19e0dce7301964f96d
ohadk123 <ohad43@gmail.com>
2024-11-15 16:53:44 +0200

    new personal library. ktypes adds useful types

---
+++ include/libok/ktypes.h
@@ -0,0 +1,17 @@
+#ifndef KTYPES_H
+#define KTYPES_H
+
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+typedef unsigned long long uint64;
+
+typedef signed char int8;
+typedef signed short int16;
+typedef signed int int32;
+typedef signed long long int64;
+
+typedef float float32;
+typedef double float64;
+
+#endif // KTYPES_H
