b00d56ca4cb819bc19d991827b17cd339cdbb63b
ohadk123 <ohad43@gmail.com>
2024-11-19 23:44:58 +0200

    added assembly compile rules to the kernel. PIC initializtion to main. Automatic VSCODE settings...

--- kernel/main.c
+++ kernel/main.c
@@ -1,14 +1,18 @@
 #include "kheaders/console.h"
 #include "kheaders/int.h"
 #include "kheaders/asm.h"
+#include "kheaders/pic.h"
 
 void main() {
-    cli();
     console_init();
     idt_init();
+    pic_init();
     sti();
     console_puts("Hello, World!\n");
-    int(0);
+    int(0x20);
+    int(0x21);
+    int(0x21);
+    int(0x22);
     cli();
     hlt();
 }
